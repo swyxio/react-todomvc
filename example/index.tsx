a48367f978d6192e8eadb8bf2fd1603b5e98d6ba
swyx <wanshawn@amazon.com>
2020-07-18 03:16:22 +0800

    example

--- example/index.tsx
+++ example/index.tsx
@@ -8,6 +8,8 @@ const App = () => {
   const props = useTodosLocalState()
   return (
     <div>
+      <h1>React-TodoMVC demo - a demo React component to demo your backend!</h1>
+      <h2><a href="https://github.com/sw-yx/react-todomvc">View source here.</a></h2>
       <Todos {...props} />
     </div>
   );
